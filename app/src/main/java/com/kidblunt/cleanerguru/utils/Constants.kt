b837649305885ef9c0d7029014a953ee39406ee2
bluntentertainment1-arch <bluntentertainment1@gmail.com>
2025-12-19 22:42:26 +0100

    Update privacy URL in Constants.kt

--- app/src/main/java/com/kidblunt/cleanerguru/utils/Constants.kt
+++ app/src/main/java/com/kidblunt/cleanerguru/utils/Constants.kt
@@ -10,5 +10,5 @@ object Constants {
     
     const val WEBSITE_URL = "https://app.lastapp.ai/appurls/marketing?app_id=5380bb5c-c23c-4a7e-acbf-07ee7a412b56"
     const val SUPPORT_URL = "https://app.lastapp.ai/appurls/support?app_id=5380bb5c-c23c-4a7e-acbf-07ee7a412b56"
-    const val PRIVACY_URL = "https://app.lastapp.ai/appurls/privacy?app_id=5380bb5c-c23c-4a7e-acbf-07ee7a412b56"
+    const val PRIVACY_URL = "https://bluntentertainment1-arch.github.io/Clean-Booster-Pro-26/privacy-policy.html"
 }
